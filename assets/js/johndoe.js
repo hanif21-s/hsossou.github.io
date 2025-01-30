c338c8f520f60d47a5a8b127f6c542499ee08a48
Hanif SOSSOU <hsossou@cfao.corp>
2025-01-30 16:39:46 +0000

    Initialisation du projet

---
+++ assets/js/johndoe.js
@@ -0,0 +1,71 @@
+/*!
+=========================================================
+* JohnDoe Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// protfolio filters
+$(window).on("load", function() {
+    var t = $(".portfolio-container");
+    t.isotope({
+        filter: ".new",
+        animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: !1
+        }
+    }), $(".filters a").click(function() {
+        $(".filters .active").removeClass("active"), $(this).addClass("active");
+        var i = $(this).attr("data-filter");
+        return t.isotope({
+            filter: i,
+            animationOptions: {
+                duration: 750,
+                easing: "linear",
+                queue: !1
+            }
+        }), !1
+    });
+});
+
+
+// google maps
+function initMap() {
+// Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 6.1933637, lng: 1.1809907},
+        zoom: 17,
+        scrollwheel:  true,
+        navigationControl: true,
+        mapTypeControl: true,
+        scaleControl: true,
+      
+    });
+}
